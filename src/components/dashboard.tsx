af72ad9043693a8a44d8782a3a00ccd3ee71f8a6
markmumba <markmumba01@gmail.com>
2024-01-28 23:03:10 +0300

    added the transactions tab

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -22,6 +22,7 @@ interface Dashboard {
 }
 
 function Dashboard(props: any) {
+    const number: number = 5
 
     const [userData, setUserData] = useState<Dashboard | null>()
 
@@ -54,11 +55,12 @@ function Dashboard(props: any) {
                                 <div className="h-screen">
                                     <SideBar fullName={userData.user.fullname} />
                                 </div>
-                                <div className="absolute left-60 ">
+                                <div className="absolute left-[30%]">
+
                                     <h1 className="text-4xl font-bold ">Dashboard</h1>
                                     <h2 className="text-slate-500">Welcome back , {userData.user.username}</h2>
                                 </div>
-                                <div className="flex space-x-10 w-full absolute pt-24 left-60 ">
+                                <div className="flex space-x-10 w-full absolute pt-24 left-[30%] ">
                                     {userData.account.map((account: AccountDetails) => (
 
                                         <div className="rounded-2xl  flex-auto max-w-sm p-6 space-y-4 text-white  shadow-lg  bg-gradient-to-br from-cyan-500  via-indigo-500 to-slate-900">
@@ -81,7 +83,7 @@ function Dashboard(props: any) {
                         </>)
                         }
 
-                        <Transaction />
+                        <Transaction number={number} />
                     </div>) :
                     (<>
                         <h1>You are logged out of the app </h1>
--- src/components/transaction.tsx
+++ src/components/transaction.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react"
 import { axiosInstance } from "../services/fetcher"
+import { formatMoneyString } from "./helper"
 
 interface Transaction {
     recepientAccount: string,
     amount: string,
     type: string
     timestamp: string
+    accountType: string
 
 }
 
-function Transaction() {
+function Transaction(props: any) {
 
     const [transactions, setTransactions] = useState<Transaction[] | null>([])
 
@@ -20,8 +22,14 @@ function Transaction() {
                 'Content-Type': 'application/json'
             }
         })
-        const content = response.data
-        setTransactions(content)
+        const content = (response.data)
+        if (props.number) {
+            const lastfive = content.slice(-props.number);
+            setTransactions(lastfive)
+        } else {
+            setTransactions(content)
+        }
+
     }
 
     useEffect(() => {
@@ -30,20 +38,26 @@ function Transaction() {
 
     return (
         <>
-            <div>
+            <div className="absolute left-[30%] w-full top-[45%]">
+                <h1 className=" font-bold text-3xl pb-3">My Transactions</h1>
                 {transactions ? (
                     <div>
                         {transactions.map((transaction, index) => (
-                            <div className="border rounded-lg flex flex-auto p-6" key={index}>
+                            <div className=" max-w-4xl rounded-lg flex flex-auto px-6 py-3 space-x-40 my-3 bg-slate-100" key={index}>
                                 <p style={{ color: transaction.type === 'deposit' ? 'green' : 'red' }}>
                                     {transaction.type === 'deposit' ? '+' : '-'}
-                                    {transaction.amount}</p>
+                                    {formatMoneyString(transaction.amount)}</p>
                                 <p>{transaction.recepientAccount ? transaction.recepientAccount : transaction.type}</p>
-                                <p>{transaction.timestamp}</p>
+                                <p>{new Date(transaction.timestamp).toLocaleString()}</p>
+                                <p>{transaction.accountType}</p>
 
                             </div>
                         ))}
+                        <div className="absolute left-[40%] p-4">
+                            <button className=" border px-6 py-3 rounded-lg bg-indigo-600 text-white">See more</button>
+                        </div>
                     </div>
+
                 )
                     : (
                         <div>
