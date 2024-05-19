30742dea4ff13883b8bd0124f01ae01a90950f67
markmumba <markmumba01@gmail.com>
2024-05-19 23:03:33 +0300

    fixing the register form and route :

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,25 +1,27 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { axiosInstance } from "../services/fetcher";
 import { useNavigate } from "react-router-dom";
-import image from "../assets/8292064.jpg"
+import image from "../assets/8292064.jpg";
 
-function Register(props :any) {
-
-    const navigate = useNavigate()
+function Register(props: any) {
+    const navigate = useNavigate();
 
     const [register, setRegister] = useState({
-        userName: "",
+        user_name: "",
         email: "",
-        fullName: "",
+        full_name: "",
         password: ""
-    })
+    });
+
+    console.log(register);
+    console.log(JSON.stringify(register));
 
     function handleChange(e: ChangeEvent<HTMLInputElement>) {
-        const { name, value } = e.target
+        const { name, value } = e.target;
         setRegister((prevRegister) => ({
             ...prevRegister,
             [name]: value
-        }))
+        }));
     }
 
     async function handleSubmit(e: FormEvent<HTMLFormElement>) {
@@ -27,7 +29,6 @@ function Register(props :any) {
 
         try {
             const requestBody = JSON.stringify(register);
-
             const response = await axiosInstance.post("user/create", requestBody, {
                 headers: {
                     'Content-Type': 'application/json'
@@ -35,37 +36,35 @@ function Register(props :any) {
             });
 
             if (response) {
-                props.setAccount(true)
-                navigate("/login")
+                props.setAccount(true);
+                navigate("/login");
             }
 
         } catch (error) {
             console.error('Error :', error);
         }
-
-    };
-
+    }
 
     return (
         <>
             <div className="flex h-screen">
-                <div className="w-1/2 bg-cover " style={{ backgroundImage: `url(${image})` }}>
+                <div className="w-1/2 bg-cover" style={{ backgroundImage: `url(${image})` }}>
                 </div>
 
                 <div className="w-1/2 px-20 py-40 mt-20">
                     <h2 className="text-3xl font-bold mb-6">Register</h2>
                     <form onSubmit={handleSubmit}>
                         <div className="mb-4">
-                            <label htmlFor="username" className="block text-sm font-medium text-gray-600">Username</label>
-                            <input type="text" id="username" name="userName" onChange={handleChange} value={register.userName} className="mt-1 p-3 rounded-md bg-gray-200 text-black w-full" />
+                            <label htmlFor="user_name" className="block text-sm font-medium text-gray-600">Username</label>
+                            <input type="text" id="user_name" name="user_name" onChange={handleChange} value={register.user_name} className="mt-1 p-3 rounded-md bg-gray-200 text-black w-full" />
                         </div>
                         <div className="mb-4">
                             <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email</label>
                             <input type="email" id="email" name="email" onChange={handleChange} value={register.email} className="mt-1 p-3 rounded-md bg-gray-200 text-black w-full" />
                         </div>
                         <div className="mb-4">
-                            <label htmlFor="fullname" className="block text-sm font-medium text-gray-600">Full Name</label>
-                            <input type="text" id="fullname" name="fullName" onChange={handleChange} value={register.fullName} className="mt-1 p-3 rounded-md bg-gray-200 text-black w-full" />
+                            <label htmlFor="full_name" className="block text-sm font-medium text-gray-600">Full Name</label>
+                            <input type="text" id="full_name" name="full_name" onChange={handleChange} value={register.full_name} className="mt-1 p-3 rounded-md bg-gray-200 text-black w-full" />
                         </div>
                         <div className="mb-4">
                             <label htmlFor="password" className="block text-sm font-medium text-gray-600">Password</label>
@@ -75,10 +74,8 @@ function Register(props :any) {
                     </form>
                 </div>
             </div>
-
         </>
-    )
-
+    );
 }
 
 export default Register;
